43018751e875c1e71db3fc71a580ddb4f05917e1
Marsietis <martynas.matijosius@gmail.com>
2023-09-12 16:48:50 +0300

    Added logic for displaying pressed button in display

--- app/src/main/java/com/example/mipt_pd3/MainActivity.kt
+++ app/src/main/java/com/example/mipt_pd3/MainActivity.kt
@@ -2,10 +2,64 @@ package com.example.mipt_pd3
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.core.text.isDigitsOnly
 
 class MainActivity : AppCompatActivity() {
+
+    private var currentExpression = ""
+    private lateinit var calculatorDisplay: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        calculatorDisplay = findViewById(R.id.display)
+
+        val buttonIds = listOf(
+            R.id._0,
+            R.id._1,
+            R.id._2,
+            R.id._3,
+            R.id._4,
+            R.id._5,
+            R.id._6,
+            R.id._7,
+            R.id._8,
+            R.id._9,
+            R.id.plus,
+            R.id.minus,
+            R.id.multiply,
+            R.id.divide,
+            R.id.squareRoot,
+            R.id.deleteSymbol,
+            R.id.clear,
+            R.id.clearEntry,
+            R.id.changeSymbol
+        )
+
+        for (buttonId in buttonIds) {
+            val button = findViewById<Button>(buttonId)
+            button.setOnClickListener {
+                handleButtonClick(button)
+            }
+        }
+    }
+
+    private fun handleButtonClick(button: Button) {
+        val buttonText = button.text.toString()
+
+        if (buttonText.isDigitsOnly()){
+            currentExpression += buttonText
+        }
+        else if (buttonText == "+" || buttonText == "-" || buttonText == "*" || buttonText == "/"){
+            currentExpression += " $buttonText "
+        }
+        updateDisplay()
+    }
+
+    private fun updateDisplay(){
+        calculatorDisplay.text = currentExpression
     }
 }

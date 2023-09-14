dc8928d393643a157220a383aabc41fa7e9ea4bf
Marsietis <martynas.matijosius@gmail.com>
2023-09-14 18:58:37 +0300

    Updated calculation logic, added error handling.

--- app/src/main/java/com/example/mipt_pd3/Calculate.kt
+++ app/src/main/java/com/example/mipt_pd3/Calculate.kt
@@ -3,36 +3,69 @@ package com.example.mipt_pd3
 import kotlin.math.sqrt
 
 object Calculate {
-    fun changeSymbol(expression: String): String {
-        return if (expression.startsWith("-")) {
-            expression.substring(1)
+    fun changeSymbol(inputtedNumber: String): String {
+        if (inputtedNumber == "0" || inputtedNumber == "") {
+            return inputtedNumber
+        }
+        return if (inputtedNumber.startsWith("-")) {
+            inputtedNumber.substring(1)
         } else {
-            "-$expression"
+            "-$inputtedNumber"
+        }
+
+    }
+
+    fun calculateExpression(
+        currentExpression: String,
+        inputtedNumber: String,
+        pendingSymbol: Char
+    ): String {
+        return when (pendingSymbol) {
+            '+' -> plus(currentExpression, inputtedNumber)
+            '-' -> minus(currentExpression, inputtedNumber)
+            '*' -> multiply(currentExpression, inputtedNumber)
+            '/' -> divide(currentExpression, inputtedNumber)
+            else -> currentExpression
         }
     }
 
-    fun plus(currentExpression: String, inputtedNumber: String): String {
+    private fun plus(currentExpression: String, inputtedNumber: String): String {
         val result = currentExpression.toDouble() + inputtedNumber.toDouble()
         return result.toString()
     }
 
-    fun minus(currentExpression: String, inputtedNumber: String): String {
+    private fun minus(currentExpression: String, inputtedNumber: String): String {
         val result = currentExpression.toDouble() - inputtedNumber.toDouble()
         return result.toString()
     }
 
-    fun multiply(currentExpression: String, inputtedNumber: String): String {
+    private fun multiply(currentExpression: String, inputtedNumber: String): String {
         val result = currentExpression.toDouble() * inputtedNumber.toDouble()
         return result.toString()
     }
 
-    fun divide(currentExpression: String, inputtedNumber: String): String {
-        val result = currentExpression.toDouble() / inputtedNumber.toDouble()
-        return result.toString()
+    private fun divide(currentExpression: String, inputtedNumber: String): String {
+        try {
+            val numerator = currentExpression.toDouble()
+            val denominator = inputtedNumber.toDouble()
+
+            if (denominator == 0.0) {
+                return "Error"
+            }
+
+            val result = numerator / denominator
+            return result.toString()
+        } catch (e: NumberFormatException) {
+            return "Error"
+        }
     }
 
-    fun squareRoot(currentExpression: String): String {
-        val result = sqrt(currentExpression.toDouble())
-        return result.toString()
+    fun squareRoot(inputtedNumber: String): String {
+        return try {
+            val result = sqrt(inputtedNumber.toDouble())
+            result.toString()
+        } catch (e: NumberFormatException) {
+            "Error"
+        }
     }
 }
